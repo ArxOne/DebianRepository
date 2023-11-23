7b1971ce3dc4978bdaa052a27e5315e92e8f7dbd
Pascal Craponne <pascal.craponne@arx.one>
2023-11-23 18:18:46 +0100

    Code cleanup

--- DebianRepository/DebianRepositoryRoute.cs
+++ DebianRepository/DebianRepositoryRoute.cs
@@ -2,12 +2,4 @@
 
 namespace ArxOne.Debian;
 
-public record DebianRepositoryRoute(string Path, Delegate? Handler, string? ContentType, string? Redirection = null)
-{
-    public void Deconstruct(out string path, out Delegate? handler, out string? contentType)
-    {
-        path = Path;
-        handler = Handler;
-        contentType = ContentType;
-    }
-}
+public record DebianRepositoryRoute(string Path, Delegate? Handler, string? ContentType, string? Redirection = null);
